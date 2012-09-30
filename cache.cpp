afae5b978e43532c3cc487af688ed2ab16ea6ddc
Giuliano <gs93@gmx.net>
2012-09-30 21:14:42 +0200

    class to cache the function calls

---
+++ cache.cpp
@@ -0,0 +1,20 @@
+#include "cache.hpp"
+
+cache::cache()
+{
+}
+
+void cache::add(statusfunc_t *f, unsigned int calls)
+{
+    _cacheFunc tmp;
+    tmp.func = f;
+    tmp.calls = calls;
+    tmp.lastCall = 1;
+    tmp.lastResult = f();
+    functions.push_back(tmp);
+}
+
+void cache::execute()
+{
+    // TODO: write this..^^
+}
---
+++ cache.hpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef string statusfunc_t();
+
+class cache {
+public:
+    cache();
+    void add(statusfunc_t *f, unsigned int calls);
+    void execute();
+    virtual ~cache();
+private:
+    struct _cacheFunc {
+        statusfunc_t *func;
+        unsigned int calls;
+        unsigned int lastCall;
+        string lastResult;
+    };
+    vector<_cacheFunc> functions;
+};
